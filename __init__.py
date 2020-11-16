adcc24ac6c77cdf6a577e2acba26587a58382ada
Sean <sean-mullan@uiowa.edu>
2020-11-16 14:05:34 -0600

    change Loggers to Logging

--- __init__.py
+++ __init__.py
@@ -9,4 +9,4 @@ from .model_arch import get_model_func
 from .learning_rates import get_lr_func
 from .train_functions import get_update_step
 from .core_model import CoreModel
-from .Loggers import TensorboardLoggingHandler, FileLoggingHandler
+from .Logging import TensorboardLoggingHandler, FileLoggingHandler
