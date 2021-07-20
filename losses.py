e6a015792055878051dfafa47b1b9044cfbe5c1c
Sean Mullan <sean@seanmullan.com>
2021-07-20 11:49:37 -0500

    update loss reductions and add combinations

--- losses.py
+++ losses.py
@@ -1,6 +1,22 @@
-from functools import partial
-
 import tensorflow as tf
+
+
+def get_reduction(reduction, batch_size=None):
+    if reduction == 'sum_over_batch_size' or reduction == 'auto':
+        if batch_size is None:
+            def reduction_func(x):
+                return tf.reduce_mean(x)
+        else:
+            def reduction_func(x):
+                return tf.reduce_sum(x) * (1. / batch_size)
+    elif reduction == 'sum':
+        reduction_func = tf.reduce_sum
+    elif reduction == 'none':
+        def reduction_func(x):
+            return x
+    else:
+        raise ValueError("Unknown reduction method")
+    return reduction_func
 
 
 def IOU_loss(**kwargs):
@@ -15,17 +31,20 @@ def IOU_loss(**kwargs):
     return tf.function(loss)
 
 
-def dice_loss(as_scalar=True, **kwargs):
+def dice_loss(as_scalar=True, reduction='sum_over_batch_size', **kwargs):
+    reduction_func = get_reduction(reduction)
     if as_scalar:
         def loss(y_true, y_pred):
-            numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=(1, 2, 3))
-            denominator = tf.reduce_sum(y_true + y_pred, axis=(1, 2, 3))
-            return 1 - numerator / denominator
+            numerator = 2 * tf.reduce_sum(tf.reshape(y_true * y_pred, [y_true.shape[0], -1]), axis=1)
+            denominator = tf.reduce_sum(tf.reshape(y_true + y_pred, [y_true.shape[0], -1]), axis=1)
+            dice = 1 - numerator / denominator
+            return reduction_func(dice)
     else:
         def loss(y_true, y_pred):
             numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=-1)
             denominator = tf.reduce_sum(y_true + y_pred, axis=-1)
-            return 1 - (numerator + denominator) / (denominator + 1)
+            dice = 1 - (numerator + denominator) / (denominator + 1)
+            return reduction_func(dice)
     return tf.function(loss)
 
 
@@ -59,21 +78,13 @@ def mixed_IOU_BCE_loss(beta=0.5, label_smoothing=0.1, reduction='auto', **kwargs
 
 def focal_loss(beta=0.25, gamma=2, reduction='sum_over_batch_size', **kwargs):
     """https://arxiv.org/abs/1708.02002 - code adapted from https://lars76.github.io/neural-networks/object-detection/losses-for-segmentation/"""
+    reduction_func = get_reduction(reduction)
+
     def focal_loss_with_logits(logits, targets, beta, gamma, y_pred):
         weight_a = beta * (1 - y_pred) ** gamma * targets
         weight_b = (1 - beta) * y_pred ** gamma * (1 - targets)
 
         return (tf.math.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b
-
-    if reduction == 'sum_over_batch_size' or reduction == 'auto':
-        reduction_func = partial(tf.reduce_mean, axis=(1, 2, 3))  # NOTE: This assumes [batch, x, y, channels]
-    elif reduction == 'sum':
-        reduction_func = tf.reduce_sum
-    elif reduction == 'none':
-        def reduction_func(x):
-            return x
-    else:
-        raise ValueError("Unknown reduction method")
 
     def loss(y_true, y_pred):
         y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
@@ -102,6 +113,49 @@ def weighted_crossentropy(pos_weight=1, label_smoothing=0, **kwargs):
         loss = tf.nn.weighted_cross_entropy_with_logits(smoothed_y, y_pred, pos_weight)
         return tf.reduce_mean(loss)
     return weighted_bce
+
+
+def get_combined_losses(*losses, divide_by_total_weight=False, **loss_kwargs):
+    """Get a loss function by combining other loss functions.
+
+    Parameters
+    ----------
+    losses : tuple(str or func or numeric)
+        The losses (and optional weights) to combine
+    divide_by_total_weight : bool
+        Whether to return the final loss as a weighted average
+    loss_kwargs : dict
+        Any other keywords for the loss functions
+
+    NOTE
+    ----
+    losses should be each loss function followed by the optional weight as an int or float. If an int or float doesn't follow a loss function, then it will have a weight of 1
+    For example: get_combined_losses('bce', 2, 'dice', 3) -> bce loss * 2 + dice loss * 3
+    Alternatively: get_combined_losses('bce', 'dice', 3) -> bce loss * 1 + dice loss * 3
+    """
+    loss_funcs = []
+    total_weight = 0
+    i = 0
+    while i < len(losses):
+        loss = losses[i]
+        weight = losses[i + 1] if i + 1 < len(losses) else 1
+        if not isinstance(weight, (int, float)):
+            i -= 1
+            weight = 1
+        total_weight += weight
+        if isinstance(loss, str):
+            loss = get_loss_func(loss)(**loss_kwargs)
+        loss_funcs.append([loss, weight])
+        i += 2
+
+    def loss_func(y_true, y_pred):
+        total_loss = 0
+        for func, weight in loss_funcs:
+            total_loss += func(y_true, y_pred) * weight
+        if divide_by_total_weight:
+            total_loss /= total_weight
+        return total_loss
+    return tf.function(loss_func)
 
 
 def get_loss_func(loss_type, **kwargs):
